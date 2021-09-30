9b637a62aeca60aa844a73e673a30927901317dc
ar sharma <akssharma1994@gmail.com>
2021-09-30 13:23:17 +0530

    updating jenkins file

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
+from flask import Flask, render_template
+
 app = Flask(__name__)
-
-# list of cat images
-image_urls_map = {
-    "https://cdac-project.s3-us-west-2.amazonaws.com/compressed_jenkins-min.jpg": "https://cdac-project.s3-us-west-2.amazonaws.com/IMG_9086.JPG",
-    "https://cdac-project.s3-us-west-2.amazonaws.com/compressed_jenkins-min.jpg": "https://cdac-project.s3-us-west-2.amazonaws.com/IMG_9086.JPG",
-}
-
 
 @app.route("/")
 def index():
-    key = random.choice(list(image_urls_map))
-    return render_template("index.html", cdac_project_url=image_urls_map, image_url=key)
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
